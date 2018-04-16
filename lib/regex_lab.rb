ea8a09bd5e05b164da5f06b73fcdc64a43932af6
Brent Bauman <brentbauman86@gmail.com>
2018-04-16 15:34:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,10 +11,9 @@ text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/[A-Z]\w+[[:punct:]]/) ? true : false
+text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
 
 end
